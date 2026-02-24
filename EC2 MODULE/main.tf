69ee24f7e75a5d5e37c85191b55460c9fbf590ea
Decsika Helen Talluri <helendecsika5@gmail.com>
2026-02-24 22:07:17 +0530

    main.tf

---
+++ EC2 MODULE/main.tf
@@ -0,0 +1,76 @@
+resource "aws_security_group" "web_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web1" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              echo "Hello from Terraform Module Web Server" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+# LOAD BALANCER
+
+resource "aws_lb" "alb" {
+  name               = "module-alb"
+  load_balancer_type = "application"
+  subnets            = [var.subnet_id]
+  security_groups    = [aws_security_group.web_sg.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "module-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
